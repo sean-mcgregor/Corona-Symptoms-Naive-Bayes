b322b55526a471fbcaa31b43aa68f0194cf7119b
sean-mcgregor <worksmcg@gmail.com>
2020-04-07 17:13:57 +0100

    Added calculateProbabilities()
    It goes through elements of arrayOfTokens and updates variables to
    reflect the data. As of now there are still errors in the method and the
    counters do not correctly represent the data.

--- coronaVirusSymptomsNaiveBayers/src/coronaVirusSymptomsNaiveBayers/Dataset.java
+++ coronaVirusSymptomsNaiveBayers/src/coronaVirusSymptomsNaiveBayers/Dataset.java
@@ -6,6 +6,8 @@ public class Dataset {
 	
 	ArrayList<String> arrayOfTokens;
 	
+	private boolean hasCovid;
+	
 	private int temperatureHotAndCovid = 0;
 	private int temperatureHotAndNoCovid = 0;
 	private int temperatureNormalAndCovid = 0;
@@ -36,15 +38,218 @@ public class Dataset {
 	private int wasNotInDangerZoneAndNoCovid= 0;
 	
 	
-	public Dataset(ArrayList<String> arrayOfTokens) {
+	public Dataset(ArrayList <String> arrayOfTokens) {
 		
 		this.setArrayOfTokens(arrayOfTokens);
 		
-		for(String element: arrayOfTokens)
-			System.out.println(element);
+		this.calculateProbabilities(arrayOfTokens);
+		
+		System.out.println(temperatureHotAndCovid);
+		System.out.println(temperatureHotAndNoCovid);
+		System.out.println(temperatureNormalAndCovid);
+		System.out.println(temperatureNormalAndNoCovid);
+		System.out.println(temperatureCoolAndCovid);
+		System.out.println(temperatureCoolAndNoCovid);
+		System.out.println(temperatureColdAndCovid);
+		System.out.println(temperatureColdandNoCovid);
+		
+		System.out.println(wasInDangerZoneAndCovid);
+		System.out.println(wasInDangerZoneAndNoCovid);
+		System.out.println(wasNotInDangerZoneAndCovid);
+		System.out.println(wasNotInDangerZoneAndNoCovid);
+		
 	}
-
-
+	
+	/**
+	 * Calculates the probabilities to be used in Naive Baye's from the data set provided.
+	 * 
+	 * @param arrayOfTokens each element in the array represents one patient and all of their symptoms.
+	 */
+	public void calculateProbabilities(ArrayList <String> arrayOfTokens) {
+		
+		for(int i = 0; i < arrayOfTokens.size(); i++) {
+						
+			String[] element = arrayOfTokens.get(i).split(" ");
+			
+			//checking if the patient has Covid
+			if(element[5].equals("yes")) {
+				
+				setHasCovid(true);
+			}
+			else {
+				
+				setHasCovid(false);
+			}
+			
+				
+			if (hasCovid()) {
+				
+				//passing through each symptom and incrementing variables to reflect the status of the symptom
+				for(int j = 0; j < (element.length - 1); j++) {
+				
+					switch(j) {
+					
+						case 0:
+							
+							switch(element[0]) {
+							
+								case "hot":
+									setTemperatureHotAndCovid(getTemperatureHotAndCovid() + 1);
+									break;
+									
+								case "normal":
+									setTemperatureNormalAndCovid(getTemperatureNormalAndCovid() + 1);
+									break;
+									
+								case "cool":
+									setTemperatureCoolAndCovid(getTemperatureCoolAndCovid() + 1);
+									break;
+									
+								case "cold":
+									setTemperatureColdAndCovid(getTemperatureColdAndCovid() + 1);
+									break;
+								 
+							}
+							
+						case 1:
+							
+							switch(element[1]) {
+								
+								case "yes":
+									setAchesAndCovid(getAchesAndCovid() + 1);
+									break;
+									
+								case "no":
+									setNoAchesAndCovid(getNoAchesAndCovid() + 1);
+									break;
+							}
+							
+						case 2:
+							
+							switch(element[2]) {
+							
+								case "yes":
+									setCoughAndCovid(getCoughAndCovid() + 1);
+									break;
+									
+								case "no":
+									setNoCoughAndCovid(getNoCoughAndCovid() + 1);
+									break;
+							}
+							
+						case 3:
+							
+							switch(element[3]) {
+							
+								case "yes":
+									setSoreThroatAndCovid(getSoreThroatAndCovid() + 1);
+									break;
+									
+								case "no":
+									setNoSoreThroatAndCovid(getNoSoreThroatAndCovid() + 1);
+									break;
+							}
+							
+						case 4:
+							
+							switch(element[4]) {
+							
+								case "yes":
+									setWasInDangerZoneAndCovid(getWasInDangerZoneAndCovid() + 1);
+									break;
+									
+								case "no":
+									setWasNotInDangerZoneAndCovid(getWasNotInDangerZoneAndCovid() + 1);
+									break;
+							}
+					}
+				}
+			}
+			else {
+				
+				//passing through each symptom and incrementing variables to reflect the status of the symptom
+				for(int j = 0; j < (element.length - 1); j++) {
+				
+					switch(j) {
+					
+						case 0:
+							
+							switch(element[0]) {
+							
+								case "hot":
+									setTemperatureHotAndNoCovid(getTemperatureHotAndNoCovid() + 1);
+									break;
+									
+								case "normal":
+									setTemperatureNormalAndNoCovid(getTemperatureNormalAndNoCovid() + 1);
+									break;
+									
+								case "cool":
+									setTemperatureCoolAndNoCovid(getTemperatureCoolAndNoCovid() + 1);
+									break;
+									
+								case "cold":
+									setTemperatureColdandNoCovid(getTemperatureColdandNoCovid() + 1);
+									break;
+								 
+							}
+							
+						case 1:
+							
+							switch(element[1]) {
+								
+								case "yes":
+									setAchesAndNoCovid(getAchesAndNoCovid() + 1);
+									break;
+									
+								case "no":
+									setNoAchesAndNoCovid(getNoAchesAndNoCovid() + 1);
+									break;
+							}
+							
+						case 2:
+							
+							switch(element[2]) {
+							
+								case "yes":
+									setCoughAndNoCovid(getCoughAndNoCovid() + 1);
+									break;
+									
+								case "no":
+									setNoCoughAndNoCovid(getNoCoughAndNoCovid() + 1);
+									break;
+							}
+							
+						case 3:
+							
+							switch(element[3]) {
+							
+								case "yes":
+									setSoreThroatAndNoCovid(getSoreThroatAndNoCovid() + 1);
+									break;
+									
+								case "no":
+									setNoSoreThroatAndNoCovid(getNoSoreThroatAndNoCovid() + 1);
+									break;
+							}
+							
+						case 4:
+							
+							switch(element[4]) {
+							
+								case "yes":
+									setWasInDangerZoneAndNoCovid(getWasInDangerZoneAndNoCovid() + 1);
+									break;
+									
+								case "no":
+									setWasNotInDangerZoneAndNoCovid(getWasNotInDangerZoneAndNoCovid() + 1);
+									break;
+							}
+					}
+				}
+			}
+		}
+	}
 	/**
 	 * @return the arrayOfTokens
 	 */
@@ -442,5 +647,19 @@ public class Dataset {
 	 */
 	public void setWasNotInDangerZoneAndNoCovid(int wasNotInDangerZoneAndNoCovid) {
 		this.wasNotInDangerZoneAndNoCovid = wasNotInDangerZoneAndNoCovid;
+	}
+
+	/**
+	 * @return the hasCovid
+	 */
+	public boolean hasCovid() {
+		return hasCovid;
+	}
+
+	/**
+	 * @param hasCovid the hasCovid to set
+	 */
+	public void setHasCovid(boolean hasCovid) {
+		this.hasCovid = hasCovid;
 	}
 }
